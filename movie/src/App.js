18ec43ac1c01359e4a2aaffe289bc927360b2ccd
SuperFullStack <super.fullstack@outlook.com>
2021-10-26 16:11:56 +0530

    started working on ui part of the application

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/movie'
 
 function App() {
   return (
     <div className="App">
         <h1>Hello world</h1>
+        <Movie/>
     </div>
   );
 }
---
+++ movie/src/components/movie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Movie() {
+    const movi=[1,2,3]
+    return (
+        <>
+        {
+            movi.map((movie) =>(
+                <div>
+                <h1>Hai hello</h1>
+            </div>
+    ))
+        }
+        </>
+        
+    )
+}
