26e3a87e0ab104aa8471b5b72fc747d0522553f6
SuperFullStack <super.fullstack@outlook.com>
2021-10-27 09:31:07 +0530

    started working on api in movie app

--- movie/src/components/movie.js
+++ movie/src/components/movie.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 export default function Movie() {
-    const movi=[1,2,3]
+    
+    const featured_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+    const imageurl="https://image.tmdb.org/t/p/w1280";
+    const searchurl="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
     return (
         <>
-        {
-            movi.map((movie) =>(
+        
                 <div>
                 <h1>Hai hello</h1>
             </div>
-    ))
-        }
+    
+        
         </>
         
     )
