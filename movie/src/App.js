f624c43e09f4505e1933920e147ad3df537d43fb
SuperFullStack <super.fullstack@outlook.com>
2021-10-28 17:03:47 +0530

    made fetching api and displaying the same in the ui of the user

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Movie from './components/movie'
 
 function App() {
+  const featured_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+    const imageurl="https://image.tmdb.org/t/p/w1280";
+    const searchurl="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+    const [movies,setmovies]=useState([]);
+
+
+    useEffect(()=>{
+        fetch(featured_api)
+        .then((res)=> res.json())
+        .then((data)=>{
+            console.log(data);
+            setmovies(data.results)
+        })
+    },[]);
+
+
   return (
-    <div className="App">
-        <h1>Hello world</h1>
-        <Movie/>
+    <div className="movie-container">
+      {movies.length>0 && movies.map((movie)=>
+    
+       <Movie key={movie.id} {...movie}/>
+      )}
+ 
+       
     </div>
   );
 }
--- movie/src/components/movie.js
+++ movie/src/components/movie.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-export default function Movie() {
-    
-    const featured_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+export default function Movie({title,poster_path,overview,vote_average}) {
     const imageurl="https://image.tmdb.org/t/p/w1280";
-    const searchurl="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+    
     return (
         <>
-        
-                <div>
-                <h1>Hai hello</h1>
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <div className="minfo">
+                <img src={imageurl + poster_path}/>
+                </div>
+                <h6>Ratings - {vote_average}</h6>
+                <div className="overview">
+                    <h5>{overview}</h5>
+                </div>
             </div>
-    
-        
         </>
         
     )
