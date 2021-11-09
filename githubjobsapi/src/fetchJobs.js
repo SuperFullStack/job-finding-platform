8a466b59c566b57cef441f4ad4ca74bf905a3bcc
SuperFullStack <super.fullstack@outlook.com>
2021-11-09 08:51:50 +0530

    fetch jobs funciton upgrading going on

--- githubjobsapi/src/fetchJobs.js
+++ githubjobsapi/src/fetchJobs.js
@@ -1,5 +1,9 @@
 import { useReducer } from "react"
 
+function reducer(state,action){
+
+}
+
 
 export default function fetchJobs(params, page){
     const [state,dispatch]=useReducer(reducer,{jobs:[],loading:true})
