45eb21051a5f29b717fe2ce465d43df614b1fe83
SuperFullStack <super.fullstack@outlook.com>
2021-11-09 16:41:31 +0530

    fetch jobs funciton upgrading going on

--- githubjobsapi/src/App.js
+++ githubjobsapi/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import fetchJobs from './fetchJobs';
+import {Container} from 'react-bootstrap'
+
 
 function App() {
+
+  const {jobs, loading, error}= fetchJobs();
   return (
-    <div className="App">
-   <h1>Hello world</h1>
-    </div>
+   <Container>
+     {loading && <h1>Loading...</h1>}
+     {error && <h2>Error. Try refreshing ...</h2>}
+     {<h1>{jobs.length}</h1>}
+   </Container>
   );
 }
 
--- githubjobsapi/src/fetchJobs.js
+++ githubjobsapi/src/fetchJobs.js
@@ -1,5 +1,7 @@
 export default function fetchJobs(params, page){
-
-
-    
+    return {
+        jobs:[],
+        loading:false,
+        error:false,
+    }
 }
