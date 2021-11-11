612567804452553536e9d1b7480b3660d200e5d6
SuperFullStack <super.fullstack@outlook.com>
2021-11-11 08:55:53 +0530

    fetchjobs page usereducer implementing

--- githubjobsapi/src/fetchJobs.js
+++ githubjobsapi/src/fetchJobs.js
@@ -1,7 +1,21 @@
 import { useReducer } from "react"
 
+const ACTIONS={
+    MAKE_REQUEST: 'make-request',
+    GET_DATA:'get-data',
+    ERROR:'error'
+}
 function reducer(state,action){
+    switch(action.type){
+        case ACTIONS.MAKE_REQUEST:
 
+        case ACTIONS.GET_DATA:
+
+        case ACTIONS.ERROR:
+
+        default:
+            return state
+    }
 }
 
 
