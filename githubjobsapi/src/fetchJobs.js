9cefbcad980ec618e500d11227bb5336cab79c00
SuperFullStack <super.fullstack@outlook.com>
2021-11-12 09:11:19 +0530

    fetchjobs API implementing

--- githubjobsapi/src/fetchJobs.js
+++ githubjobsapi/src/fetchJobs.js
@@ -23,7 +23,7 @@ function reducer(state,action){
 
 
 export default function fetchJobs(params, page){
-    const [state,dispatch]=useReducer(reducer,{jobs:[],loading:true})
+    const [state,dispatch]=useReducer(reducer,{ jobs:[], loading:true })
 
     useEffect(()=>{
         dispatch({type: ACTIONS.MAKE_REQUEST})
