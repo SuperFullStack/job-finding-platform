053418bd3fdf25f25e59e8a60c2986c7261cdb9f
SuperFullStack <super.fullstack@outlook.com>
2021-11-17 22:41:34 +0530

    working on expense tracker app and CSS from brad traversy

--- pro2/src/components/Header.js
+++ pro2/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <h2>
-            You are now in header section
+         Expense Tracker
         </h2>
     )
 }
