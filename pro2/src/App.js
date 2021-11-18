3951042a48c0d83b7c5d0a571a19e57b8fb1edc8
SuperFullStack <super.fullstack@outlook.com>
2021-11-18 22:47:42 +0530

    working on expense tracker app and income expenses component

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpense from './components/IncomeExpense'
+
 function App() {
   return (
     <div>
       <Header/>
         <div class="container">
             <Balance/>
+            <IncomeExpense/>
         </div>
     </div>
   );
