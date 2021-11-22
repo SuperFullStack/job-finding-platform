1270c237deba66f63f475a3113c9e29f57bbe178
SuperFullStack <super.fullstack@outlook.com>
2021-11-22 08:06:56 +0530

    working on expense tracker app and added dummy transactions

--- pro2/src/context/Globalstate.js
+++ pro2/src/context/Globalstate.js
@@ -1,5 +1,10 @@
 import React, {createContext, useReducers} from 'react';
 
 const iitialState = {
-    transactions:
+    transactions:[
+        {id:1, text:'Flower',amount:-20},
+        {id:2, text:'Salary', amount:300},
+        {id:3, text:'Book', amount:-10},
+        {id:4, text:'Camers', amount:150}
+    ]
 }
