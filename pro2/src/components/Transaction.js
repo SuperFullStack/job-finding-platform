5b76f15e0bcd64197db36b320d4b8cfc76b8b5ad
SuperFullStack <super.fullstack@outlook.com>
2021-12-07 09:02:38 +0530

    working on expense tracker app and added dummy transactions

--- pro2/src/components/Transaction.js
+++ pro2/src/components/Transaction.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Transaction({transaction}) {
+    const sign=transaction.amount<0?'-':'+';
     return (
         <div>
             <li class="minus">
